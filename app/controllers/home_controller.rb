8ff887bb448619b68c7921d2d7dca69faac085c7
GERMAN CABEZAS <GERMAN.CABEZAS@LIVE.COM>
2017-02-04 09:14:25 -0600

    Sets home page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  def index
+  end
+end
+
+#
+# class HomePagesController < ApplicationController
+#   layout 'home_page'
+#   def index
+#
+#
+#   end
+#
+#   def about
+#
+#
+#   end
+# end
