3c3432b84c3b6458e941c01311f26b386670e2b4
GERMAN CABEZAS <GERMAN.CABEZAS@LIVE.COM>
2017-02-11 05:57:55 -0600

    Configures Devise to have custom fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204155158) do
+ActiveRecord::Schema.define(version: 20170211115443) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170204155158) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
